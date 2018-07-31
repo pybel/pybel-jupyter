d3ca7dcd3fd97fbb3c3c83f8044b8fb8b8c3958f
Charles Tapley Hoyt <cthoyt@wlan-224.scai.fraunhofer.de>
2018-07-31 16:51:54 +0200

    Bump version: 0.1.0-dev → 0.1.0

--- src/pybel_jupyter/__init__.py
+++ src/pybel_jupyter/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
     'to_jupyter_str',
 ]
 
-__version__ = '0.1.0-dev'
+__version__ = '0.1.0'
 
 __title__ = 'pybel_jupyter'
 __description__ = 'A PyBEL extension for Jupyter notebooks'
